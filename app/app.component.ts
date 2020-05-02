21978287cc673d789c0fd3613cac3aa2a20768b9
donkeykong91 <darylnator@gmail.com>
2020-05-02 05:19:29 -0700

    added another ng-content to project a button at the bottom of the component

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,10 +9,16 @@ import { User } from './auth-form/auth-form.interface';
       <auth-form 
         (submitted)="createUser($event)">
         <h3>Create account</h3>
+        <button type="submit">
+          join us
+        </button>
       </auth-form>
       <auth-form 
         (submitted)="loginUser($event)">
         <h3>Login</h3>
+        <button type="submit">
+          login
+        </button>
       </auth-form>
     </div>
   `
