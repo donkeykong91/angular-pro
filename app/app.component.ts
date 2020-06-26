d9607faf3abe0632013020cd9abd27cb97544101
donkeykong91 <darylnator@gmail.com>
2020-06-26 00:59:34 -0700

    Added examples of three different types of DOMs used by angular.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,14 @@
-import { Component, ViewContainerRef, ViewChild, ComponentFactoryResolver, AfterContentInit, ComponentRef, TemplateRef } from '@angular/core';
-
-import { User } from './auth-form/auth-form.interface';
-import { AuthFormComponent } from './auth-form/auth-form.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <ng-container
-        [ngTemplateOutlet]="tmpl"
-        [ngTemplateOutletContext]="ctx">
-      </ng-container>
-      <template #tmpl let-name let-location="location">
-        {{ name }} : {{ location }}
-      </template>
+      <example-one></example-one>
+      <example-two></example-two>
+      <example-three></example-three>
     </div>
   `
 })
 export class AppComponent {
-  ctx = {
-    $implicit: 'Daryl Occ',
-    location: 'Westminster, CA'
-  }
 }
