22ce6195fa882276e9b093d6bbb7e28b61381814
donkeykong91 <darylnator@gmail.com>
2020-06-26 00:30:20 -0700

    Added ng-container instead of using the API to add template information.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,40 +7,15 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
   selector: 'app-root',
   template: `
     <div>
-      <div #entry></div>
-      <template #tmpl let-name let-location="location">
-        {{ name }} : {{ location }}
+      <ng-container
+        [ngTemplateOutlet]="tmpl">
+      </ng-container>
+      <template #tmpl>
+        Daryl Occ : Westminster, CA
       </template>
     </div>
   `
 })
-export class AppComponent implements AfterContentInit{
+export class AppComponent {
 
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-  component: ComponentRef<AuthFormComponent>;
-
-  constructor(private resolver: ComponentFactoryResolver) {
-
-  }
-
-  ngAfterContentInit() {
-    this.entry.createEmbeddedView(this.tmpl, {
-      $implicit: 'Kevin Occ'
-      location: 'Westminster, CA'
-    });
-  }
-
-  loginUser(user: User) {
-    console.log('Login', user);
-  }
-
-  moveComponent() {
-    this.entry.move(this.component.hostView, 1);
-  }
-
-  destroyComponent () {
-    this.component.destroy();
-  }
 }
